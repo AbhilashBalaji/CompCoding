01e785617fae50da7db814054eafd772152b0140
AbhilashBalaji <abhilash.balaji1998@gmail.com>
2020-06-23 20:54:50 +0530

    First commit With vim lmao

---
+++ Euler/20.py
@@ -0,0 +1,20 @@
+from functools import reduce
+num = 100
+
+def fact(n):
+    fac = 1
+    for i in range(1,n+1):
+       fac *= i
+    return fac
+
+#print(fact(nm))
+sum = 0
+num_str = fact(num)
+num_str = str(num_str)
+for i in num_str:
+
+
+    sum+=int(i)
+    print(i)
+#print()
+print(sum)
