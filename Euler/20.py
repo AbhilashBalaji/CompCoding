55c683de264ba8b4d4c373b7283166e8cecb46fb
AbhilashBalaji <abhilash.balaji1998@gmail.com>
2020-06-24 12:16:53 +0530

    smoother ???

--- Euler/20.py
+++ Euler/20.py
@@ -1,21 +1,21 @@
 from functools import reduce
 num = 100
 
+
 def fact(n):
     fac = 1
-    for i in range(1,n+1):
-       fac *= i
+    for i in range(1, n+1):
+        fac *= i
     return fac
 
-#print(fact(nm))
+
+# print(fact(nm))
 sum = 0
 num_str = fact(num)
 num_str = str(num_str)
 for i in num_str:
-
-
-    sum+=int(i)
+    sum += int(i)
     print(i)
-#print()
-print(sum)
+# print()
 
+print(sum)
